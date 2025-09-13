284e1d0cd71f98aabcde43eb2c0fd4cc19e1ea66
Abhay Rooplall <u24568792@tuks.co.za>
2025-09-13 22:32:40 +0200

    Add files via upload

---
+++ src/Backend/login.php
@@ -0,0 +1,35 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Login</title>
+        <link rel="stylesheet" href="css/login.css">
+    </head>
+    <body>
+        <?php include "header.php";?>
+        <div class="signup-container">
+            <h2>Log into you account.</h2>
+            <form id="loginForm" method="POST">
+                <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input type="text" id="email" name="email" required>
+                    <span class="error" id="emailError"></span>
+                </div>
+                <div class="form-group">
+                    <label for="password">Password:</label>
+                    <input type="password" id="password" name="password" required>
+                    <span class="error" id="passwordError"></span>
+                </div>
+                <button type="submit" class="signup-btn">Login</button>
+                <div id="formError" class="error"></div>
+            </form>
+        </div>
+        <?php include "footer.php";?>   
+    </body>
+</html>
---
+++ src/Backend/register.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link rel="stylesheet" href="css/register.css">
+    <script src="js/register.js"></script>
+</head>
+<body>
+
+    <?php include 'header.php'; ?>
+
+    <div class="form-container">
+        <h1>Register</h1>
+
+        <form id="registerForm">
+
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required><br>
+
+            <label for="surname">Surname:</label>
+            <input type="text" id="surname" name="surname" required><br>
+
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required><br>
+
+            <label for="phone_number">Phone number:</label>
+            <input type="text" id="phone_number" name="phone_number" required><br>
+
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required><br>
+
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required><br>
+
+            <label for="type">User Type:</label>
+            <select id="type" name="type" required>
+            <option value="User">User</option>
+            </select><br>
+
+            <button type="submit" id="submitBtn">Sign Up</button>
+        </form>
+
+        <div id="error-message" style="display: none;"></div>
+    </div>
+
+
+</body>
+
+</html>
